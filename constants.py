860cec49c10643d725d0143609bf25fa1d8e62e3
Andrey <andrey66-66-66@yandex.ru>
2025-05-28 15:35:22 +0300

    Revert "Создание БД: сохранение и переключение дополнений"

--- constants.py
+++ constants.py
@@ -1,12 +1,3 @@
 import logging
 
-LOGGER_LEVEL = logging.DEBUG
-
-NAME_FORSAKEN_LORE = "Forsaken Lore"
-NAME_MOUNTAINS_OF_MADNESS = "Mountains of Madness"
-NAME_STRANGE_REMNANTS = "Strange Remnants"
-NAME_UNDER_THE_PYRAMIDS = "Under the Pyramids"
-NAME_SIGNS_OF_CARCOSA = "Signs of Carcosa"
-NAME_THE_DREAMLANDS = "The Dreamlands"
-NAME_CITIES_IN_RUIN = "Cities in Ruin"
-NAME_MASKS_OF_NYARLATHOTEP = "Masks of Nyarlathotep"
+LOGGER_LEVEL = logging.INFO
--- main.py
+++ main.py
@@ -1,76 +1,70 @@
 import os
-from typing import Any, Callable, Coroutine, Dict
+from typing import Any, Callable, Coroutine, Dict, Optional
 
-from db import change_expansion, get_all_users
 from dotenv import load_dotenv
-from expansions_menu import create_expansions_buttons
 from logger import LOGGER, logger_init
-from telegram import CallbackQuery, Update
+from telegram import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
-from utils import add_new_users, get_menu_buttons
 
 load_dotenv()
 
 
 async def wake_up(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    add_new_users(update.effective_user.id)
     chat = update.effective_chat
-    buttons = get_menu_buttons()
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Случайная карта", callback_data="random_card_menu"),
+            ],
+            [
+                InlineKeyboardButton("История матчей", callback_data="games_history_menu"),
+            ],
+            [
+                InlineKeyboardButton("История компаний", callback_data="companies_history_menu"),
+            ],
+            [
+                InlineKeyboardButton("Настроить дополнения", callback_data="additional_settings_menu"),
+            ],
+        ]
+    )
     LOGGER.info(f"Пользователь {chat.username} вошел в бота")
-    await context.bot.send_message(chat_id=chat.id, text="Меню:", reply_markup=buttons)
+    await context.bot.send_message(chat_id=chat.id, text="Спасибо, что включили меня", reply_markup=buttons)
 
 
 async def error(query: CallbackQuery) -> None:
-    buttons = get_menu_buttons()
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Случайная карта", callback_data="random_card_menu"),
+            ],
+            [
+                InlineKeyboardButton("История матчей", callback_data="games_history_menu"),
+            ],
+            [
+                InlineKeyboardButton("История компаний", callback_data="companies_history_menu"),
+            ],
+            [
+                InlineKeyboardButton("Настроить дополнения", callback_data="additional_settings_menu"),
+            ],
+        ]
+    )
     LOGGER.info(f"У пользователя {query.from_user.username} произошла ошибка, query: {query.data}")
+    LOGGER.debug(f"query: {query.data}")
+    LOGGER.debug(f"buttons: {buttons}")
     await query.edit_message_text(text="Что-то пошло не так, начнём с начала?", reply_markup=buttons)
 
 
-async def expansions_settings_menu(query: CallbackQuery) -> None:
-    buttons = create_expansions_buttons(query.from_user.id)
-    text = "Выбери дополнения"
-    if query.message.text == text:
-        text += " "
-    await query.edit_message_text("Выбери дополнения", reply_markup=buttons)
-
-
-async def menu(query: CallbackQuery) -> None:
-    buttons = get_menu_buttons()
-    LOGGER.info(f"Пользователь {query.from_user.username} запросил меню")
-    await query.edit_message_text(text="Меню:", reply_markup=buttons)
-
-
-async def random_card_menu(query: CallbackQuery) -> None:
-    text = " ".join(str(user_id) for user_id in get_all_users())
-    if text == "":
-        text = "Нет пользователей"
-    await query.edit_message_text(text=text)
-
-
 async def buttons_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    FUNCTIONS: Dict[str, Callable[[CallbackQuery], Coroutine[Any, Any, None]]] = {
-        "menu": menu,
-        "random_card_menu": random_card_menu,
-        "expansions_settings_menu": expansions_settings_menu,
-    }
+    FUNCTIONS: Dict[str, Callable[[CallbackQuery, Optional[str]], Coroutine[Any, Any, None]]] = {
+        "random_card_menu": ...,  # type: ignore
+    }  # type: ignore
     chat = update.effective_chat
-    add_new_users(chat.id)
     await context.bot.send_chat_action(chat_id=chat.id, action="typing")
     query = update.callback_query
     await query.answer()
     LOGGER.info(f"Пользователь {query.from_user.username} нажал кнопку {query.data}")
-    LOGGER.debug(f"query: {query.data}")
     if query.data in FUNCTIONS.keys():
-        try:
-            await FUNCTIONS[query.data](query)  # type: ignore
-        except Exception:
-            LOGGER.exception("Exception in buttons_handler")
-            LOGGER.debug(Exception)
-            await error(query)
-    elif query.data.startswith("expansion_"):
-        LOGGER.info(f"Для пользователя {query.from_user.username} выбрал дополнение {query.data[10:]}")
-        change_expansion(query.from_user.id, query.data[10:])
-        await expansions_settings_menu(query)
+        await FUNCTIONS[query.data](query)  # type: ignore
     else:
         await error(query)
 
